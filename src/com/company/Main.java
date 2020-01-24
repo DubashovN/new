2a2b879cf27805ab9a4ae5d04c55a0be9a65957a
Fa11en <fa11en@bk.ru>
2020-01-24 20:53:11 +0300

    second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,30 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        int a = 1;
-        int b = 1;
-        while (b <=20){
-            a *= 2;
-            System.out.print(a + " ");
-            a++;
-            b++;
-        }
-
-            // while
-        //2 x 1 = 2
-        //2 x 2 = 4
-        //2 x 3 = 6
-
-//        int i = 1;
-//        while (i != 11) {
-//            int j = 2;
-//            while (j != 11) {
-//                System.out.printf(j + " x " + i + " = " + (i * j) + "\t");
-//                j++;
-//            }
-//            i++;
-//            System.out.println();
-//        }
+        new Loader().start();
     }
 }
 
