b676fbec43b5f7acce340dccaeea6ab080d63cd4
Fa11en <fa11en@bk.ru>
2020-01-24 21:01:37 +0300

    second

---
+++ src/com/company/Loader.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Loader {
+    void start() {
+        apple("apple", 'p');
+    }
+
+    void vertical(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            char vertical = string.charAt(i);
+            System.out.print(vertical + "\n");
+        }
+    }
+
+    void calculate(String string, char a) {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == a)
+                count++;
+        }
+        System.out.println(count);
+    }
+
+    void apple(String string, char a) {
+        System.out.println(string);
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == a)
+            System.out.print('^');
+        }
+    }
+
+    //написать метод, который принимает на вход строку и выводит её по вертикали.
+    //метод, который принимает строку и символ и считает кол-во символов в строке.
+    //Apple
+    // ^^
+
+}
